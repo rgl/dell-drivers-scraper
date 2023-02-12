2771a71a1a8a9af49e01f43c1c49ed983ed453dc
Rui Lopes <rgl@ruilopes.com>
2023-02-12 09:47:18 +0000

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# About
+
+[![Build status](https://github.com/rgl/dell-drivers-scraper/workflows/build/badge.svg)](https://github.com/rgl/dell-drivers-scraper/actions?query=workflow%3Abuild)
+
+This scrapes the Dell Product Drivers page into a JSON data file.
+
+## Products
+
+* [OptiPlex 7060 Desktop](https://www.dell.com/support/home/en-us/product-support/product/optiplex-7060-desktop/drivers)
+
+## Data Files
+
+The code in this repository creates a `data/optiplex-7060-desktop.json` file, for example:
+
+```json
+[
+    {
+        "name": "Dell OptiPlex 7060 System BIOS",
+        "category": "BIOS",
+        "importance": "critical",
+        "date": "2023-01-12T00:00:00.000Z",
+        "url": "https://dl.dell.com/FOLDER09328185M/1/OptiPlex_7060_1.24.0.exe"
+    },
+]
+```
+
+To get the BIOS and the Intel ME Windows drivers download URLs, you can use something like:
+
+```bash
+jq -r '[.[] | select(.name == "Dell OptiPlex 7060 System BIOS")] | first' data/optiplex-7060-desktop.json
+jq -r '[.[] | select(.name == "Intel Management Engine Components Installer")] | first' data/optiplex-7060-desktop.json
+```
---
+++ main.js
@@ -0,0 +1,163 @@
+"use strict";
+
+// install dependencies:
+//
+//      npm install
+//
+// execute:
+//
+// NB to troubleshoot uncomment $env:DEBUG and set {headless:false,dumpio:true} in main.js.
+//
+//      $env:DEBUG = 'puppeteer:*'
+//      node main.js
+
+import { program } from "commander";
+import puppeteer from "puppeteer";
+import fs from "fs";
+import path from "path";
+
+function log() {
+    console.log.apply(console, [new Date().toISOString(), ...arguments]);
+}
+
+async function getDrivers(page, product) {
+    const url = `https://www.dell.com/support/home/en-us/product-support/product/${product}/drivers`;
+
+    log(`Loading ${url}...`);
+    await page.goto(url);
+
+    log("Rejecting cookies...");
+    const rejectCookiesSelector = '[aria-label="dismiss cookie message"]';
+    try {
+        await page.waitForSelector(rejectCookiesSelector, {timeout: 1500});
+        await page.click(rejectCookiesSelector);
+    } catch {
+        // ignore. this is expected in countries without cookies consent.
+    }
+
+    log("Selecting the United States/English region...");
+    const countrySelector = "div.mh-top div.country-selector";
+    const currentCountry = await page.evaluate((countrySelector) => document.querySelector(countrySelector).innerText.trim(), countrySelector);
+    if (currentCountry != "US/EN") {
+        await page.hover(`${countrySelector} a`);
+        await page.waitForSelector(`${countrySelector} [data-region-id="Americas"]`);
+        await page.click(`${countrySelector} [data-region-id="Americas"]`);
+        await page.click(`${countrySelector} a[data-locale="en-us"]`);
+        await page.waitForNavigation();
+    }
+
+    log("Waiting for the downloads table...");
+    const downloadsTableSelector = "#downloads-table";
+    await page.waitForSelector(`${downloadsTableSelector} tr:nth-child(5)`);
+    await page.waitForSelector(`${downloadsTableSelector} #paginationRow`);
+    await page.click(`${downloadsTableSelector} #paginationRow`);
+
+    log("Getting data from the downloads table...");
+    return await page.evaluate(async () => {
+        // e.g. 12 Jan 2023
+        const dateRe = /0?(?<day>[0-9]+) (?<month>[A-Za-z]+) (?<year>[0-9]+)/;
+        const dateMonths = {
+            Jan: 1,
+            Feb: 2,
+            Mar: 3,
+            Apr: 4,
+            May: 5,
+            Jun: 6,
+            Jul: 7,
+            Aug: 8,
+            Sep: 9,
+            Oct: 10,
+            Nov: 11,
+            Dec: 12,
+        }
+        function parseDate(s) {
+            const m = dateRe.exec(s);
+            const day = parseInt(m.groups.day, 10);
+            const month = dateMonths[m.groups.month];
+            const year = parseInt(m.groups.year, 10);
+            return new Date(Date.UTC(year, month - 1, day));
+        }
+
+        var data = [];
+        const els = document.querySelectorAll("#downloads-table tr.main-row");
+        for (const el of els) {
+            const columnEls = el.querySelectorAll("td");
+            if (columnEls.length < 7) {
+                continue;
+            }
+            const name = columnEls[1].innerText.trim();
+            const importance = columnEls[2].innerText.trim().toLowerCase();
+            const category = columnEls[3].innerText.trim();
+            const date = parseDate(columnEls[4].innerText.trim());
+            const url = columnEls[5].querySelector("a.btn-download-lg").getAttribute("href").trim().replaceAll(" ", "%20");
+            const driver = {
+                name: name,
+                category: category,
+                importance: importance,
+                date: date.toISOString(),
+                url: url,
+            };
+            if (false) {
+                columnEls[6].querySelector("button").click();
+                const detailsEl = el.nextSibling && document.evaluate("//a[text()='View full driver details']", el.nextSibling, null, XPathResult.ANY_TYPE, null).iterateNext();
+                const detailsUrl = detailsEl && detailsEl.getAttribute("href");
+                columnEls[6].querySelector("button").click();
+                driver.detailsUrl = detailsUrl;
+            }
+            data.push(driver);
+        }
+        return data;
+    });
+}
+
+async function main(options) {
+    var browserConfig = {
+    };
+    if (options.debug) {
+        browserConfig = {
+            ...browserConfig,
+            headless: false,
+            devtools: true,
+            slowMo: 250,
+            dumpio: false,
+        };
+    }
+
+    log("Launching the browser...");
+    const browser = await puppeteer.launch(browserConfig);
+    try {
+        log("Creating a new browser page...");
+        const page = await browser.newPage();
+        await page.setViewport({
+            width: parseInt(options.viewportSize.split('x')[0], 10),
+            height: parseInt(options.viewportSize.split('x')[1], 10),
+            deviceScaleFactor: 1,
+        });
+        log(`Launched the ${await browser.version()} browser.`);
+
+        try {
+            const product = "optiplex-7060-desktop";
+            const scrapePath = `data/${product}.json`;
+
+            log(`Scraping ${product}...`);
+            const data = await getDrivers(page, product);
+
+            log(`Saving to ${scrapePath}...`);
+            fs.mkdirSync(path.dirname(scrapePath), {recursive: true});
+            fs.writeFileSync(scrapePath, JSON.stringify(data, null, 4));
+        } finally {
+            log("Taking a screenshot...");
+            await page.screenshot({ path: options.screenshotPath, fullPage: true });
+        }
+    } finally {
+        await browser.close();
+    }
+}
+
+program
+    .option('--screenshot-path <path>', 'screenshot output path', 'screenshot.png')
+    .option('--viewport-size <size>', 'browser viewport size', '1280x720')
+    .option('--debug', 'run the browser in foreground', false)
+    .parse(process.argv);
+
+await main(program.opts());
